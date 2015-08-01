c3319a6dbe0d11a5c72a01102f999cd1fb8d7588
cmai <armadale15@gmail.com>
2015-08-01 10:23:39 +0800

    just delete unnecessary code at 2015/August/01

--- userInfo.php
+++ userInfo.php
@@ -8,10 +8,7 @@
 	$habit = $_POST['habit'];
 	$gender = $_POST['gender'];
 	$fav_food = $_POST['fav_food'];
-
-	if($name=null && $nickname=null && $habit=null &&  $gender=null && $fav_food=null)
-	   echo '<script>alert("有資訊沒寫下來唷 !");</script>';
-
+	
 	$sql="INSERT INTO userinfo (name, nickname, habit, gender, fav_food) 
     	  VALUES('$name','$nickname', '$habit', '$gender', '$fav_food')";
 
